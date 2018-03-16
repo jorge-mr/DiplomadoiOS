b63ade0e0b14b7a5a0e923c1a18cd4e1b584be29
d182_Jorge_M <jorgemr@MacBook-17.local>
2018-03-16 16:25:48 -0600

    añadi el logoo

--- Ejercicios/dates.playground/Contents.swift
+++ Ejercicios/dates.playground/Contents.swift
@@ -3,3 +3,13 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let todaydate = Date()
+let todayString = "\(todaydate)"
+print("todayString",todayString)
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd' 'HH:mm:ss"
+let dateSting = dateFormatter.string(from: todaydate)
+print("dateSting",dateSting)
+let dateString = dateFormatter.date(from: dateSting)
+print("date: ",dateString)
