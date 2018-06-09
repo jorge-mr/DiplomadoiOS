6f4ddda632f4bab1f7450e2c27ec068e87eb558e
d182_Jorge_M <jorgemr@MacBook-17.local>
2018-06-08 20:29:20 -0500

    agregue un stroke que se hace mas grande con el slider

---
+++ Ejercicios/stroke/stroke/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  stroke
+//
+//  Created by d182_Jorge_M on 08/06/18.
+//  Copyright © 2018 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var delta : CGFloat = 15.0
+    let shapeLayer = CAShapeLayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.layer.addSublayer(shapeLayer)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func drawBezier(){
+        let bezier = UIBezierPath()
+        let width = view.frame.width
+        let height = view.frame.height
+        let x = width / 2
+        let y = height / 2
+        
+        let long : CGFloat = 50
+        let p1 = CGPoint(x: x - delta, y: y - delta)
+        let p2 = CGPoint(x: x - delta, y: y - long - delta)
+        let p3 = CGPoint(x: x + long - delta, y: y - long - delta)
+        let p4 = CGPoint(x: x + 2 * long - delta, y: y - long - delta)
+        let p5 = CGPoint(x: x + 2 * long - delta, y: y - 2 * long - delta)
+        let p6 = CGPoint(x: x + 2 * long + delta, y: y - 2 * long + delta)
+        let p7 = CGPoint(x: x + 2 * long + delta, y: y - long + delta)
+        let p8 = CGPoint(x: x + long + delta, y: y - long + delta)
+        let p9 = CGPoint(x: x + delta , y: y - long + delta)
+        let p10 = CGPoint(x: x + delta , y: y + delta)
+        
+        
+        bezier.move(to: p1)
+        // bezier.addLine(to: CGPoint(x: width, y: height))
+        bezier.addCurve(to: p3, controlPoint1: p1, controlPoint2: p2)
+        bezier.addCurve(to: p5, controlPoint1: p3, controlPoint2: p4)
+        bezier.addLine(to: p6)
+        bezier.addCurve(to: p8, controlPoint1: p6, controlPoint2: p7)
+        bezier.addCurve(to: p10, controlPoint1: p8, controlPoint2: p9)
+        bezier.addLine(to: p1)
+        bezier.close()
+        shapeLayer.path = bezier.cgPath
+        shapeLayer.fillColor = UIColor.blue.cgColor
+    }
+
+    @IBAction func didMoveSlider(_ sender: UISlider) {
+        delta = CGFloat(sender.value)
+        drawBezier()
+    }
+    
+}
+
