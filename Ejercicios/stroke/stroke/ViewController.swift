d9d05b12b4fb7ee5cffdbed3a9f89497dfef8dc2
jorge-home <jorgemr@MacBook-Air-de-Jorge.local>
2018-06-08 23:53:08 -0500

    hice el stroke horizontal

--- Ejercicios/stroke/stroke/ViewController.swift
+++ Ejercicios/stroke/stroke/ViewController.swift
@@ -23,28 +23,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func hypotenuse(_ a: CGFloat) -> CGFloat {
+        return (a * a + a * a).squareRoot()
+    }
+    
     func drawBezier(){
         let bezier = UIBezierPath()
         let width = view.frame.width
         let height = view.frame.height
-        let x = width / 2
+        let x = width / 2 - 80
         let y = height / 2
         
         let long : CGFloat = 50
+//        let p1 = CGPoint(x: x - delta, y: y - delta)
+//        let p2 = CGPoint(x: x - delta, y: y - long - delta)
+//        let p3 = CGPoint(x: x + long - delta, y: y - long - delta)
+//        let p4 = CGPoint(x: x + 2 * long - delta, y: y - long - delta)
+//        let p5 = CGPoint(x: x + 2 * long - delta, y: y - 2 * long - delta)
+//        let p6 = CGPoint(x: x + 2 * long + delta, y: y - 2 * long + delta)
+//        let p7 = CGPoint(x: x + 2 * long + delta, y: y - long + delta)
+//        let p8 = CGPoint(x: x + long + delta, y: y - long + delta)
+//        let p9 = CGPoint(x: x + delta , y: y - long + delta)
+//        let p10 = CGPoint(x: x + delta , y: y + delta)
         let p1 = CGPoint(x: x - delta, y: y - delta)
-        let p2 = CGPoint(x: x - delta, y: y - long - delta)
-        let p3 = CGPoint(x: x + long - delta, y: y - long - delta)
-        let p4 = CGPoint(x: x + 2 * long - delta, y: y - long - delta)
-        let p5 = CGPoint(x: x + 2 * long - delta, y: y - 2 * long - delta)
-        let p6 = CGPoint(x: x + 2 * long + delta, y: y - 2 * long + delta)
-        let p7 = CGPoint(x: x + 2 * long + delta, y: y - long + delta)
-        let p8 = CGPoint(x: x + long + delta, y: y - long + delta)
-        let p9 = CGPoint(x: x + delta , y: y - long + delta)
+        let p2 = CGPoint(x: x + long , y: y - long - hypotenuse(delta))
+        let p3 = CGPoint(x: x + 2 * long + delta, y: y - delta)
+        let p4 = CGPoint(x: x + 3 * long , y: y + long - hypotenuse(delta))
+        let p5 = CGPoint(x: x + 4 * long - delta , y: y - delta)
+        let p6 = CGPoint(x: x + 4 * long + delta, y: y + delta)
+        let p7 = CGPoint(x: x + 3 * long , y: y + long + hypotenuse(delta))
+        let p8 = CGPoint(x: x + 2 * long - delta, y: y + delta)
+        let p9 = CGPoint(x: x + long , y: y - long + hypotenuse(delta))
         let p10 = CGPoint(x: x + delta , y: y + delta)
         
         
+        // https://mathematica.stackexchange.com/questions/28202/vary-the-thickness-of-a-plotted-function
         bezier.move(to: p1)
         // bezier.addLine(to: CGPoint(x: width, y: height))
+//        bezier.addQuadCurve(to: p3, controlPoint: p2)
+//        bezier.addQuadCurve(to: <#T##CGPoint#>, controlPoint: <#T##CGPoint#>)
+        
+        
+//        ///
         bezier.addCurve(to: p3, controlPoint1: p1, controlPoint2: p2)
         bezier.addCurve(to: p5, controlPoint1: p3, controlPoint2: p4)
         bezier.addLine(to: p6)
